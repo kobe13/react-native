beb6dc7bf881f0518d6e96dada2c279f383f8ddf
kobe13 <kobe13010@gmail.com>
2024-09-07 13:09:37 +0200

    cleanup tabs

--- test-app/app/(tabs)/ships.tsx
+++ test-app/app/(tabs)/ships.tsx
@@ -23,19 +23,21 @@ export default function Ships() {
   });
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="prism" style={styles.headerImage} />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Ships</ThemedText>
-      </ThemedView>
-      {loading && <ThemedText>Loading...</ThemedText>}
-      {error && <ThemedText>Error: {error.message}</ThemedText>}
+    <>
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+        headerImage={
+          <Ionicons size={310} name="prism" style={styles.headerImage} />
+        }
+      >
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Ships</ThemedText>
+        </ThemedView>
+        {loading && <ThemedText>Loading...</ThemedText>}
+        {error && <ThemedText>Error: {error.message}</ThemedText>}
+      </ParallaxScrollView>
       <ShipsList ships={data?.ships} />
-    </ParallaxScrollView>
+    </>
   );
 }
 
